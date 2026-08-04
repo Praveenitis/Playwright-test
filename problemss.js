8236107400bdd5f9b58d29f2d0a2eb6e358c1fc3
Praveen <praveengu19@gmail.com>
2026-08-05 02:15:30 +0530

    removed everything

--- problemss.js
+++ problemss.js
@@ -1,12 +1,2 @@
-var arr = [1,2,3,4,5,6,7,8]
-var count = 0;
 
-for(let i=0;i<arr.length;i++){
-    for(let j=i+1;j<arr.length;j++){
-        if(arr[i] + arr[j] === 10){
-            count++;
-        }
-    }
-}
 
-console.log(count)
