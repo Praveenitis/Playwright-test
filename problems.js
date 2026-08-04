0e298baabbba5b878a82066b136a0e2189593ac2
Praveen <praveengu19@gmail.com>
2026-08-05 02:14:28 +0530

    new problems solved

--- problems.js
+++ problems.js
@@ -66,6 +66,103 @@ for(let i=0;i<arr.length;i++){
 console.log(count)
 
 
+//Anagram 
+
+function sortstr(str){
+    
+
+    return str.split('').sort().join('')
+}
+
+var str1 = "listen"
+var str2 = "silent"
+
+var sortedstr1 = sortstr(str1)
+var sortedstr2 = sortstr(str2)
+
+if(sortedstr1===sortedstr2){
+    console.log("Anagram")
+} else {
+    console.log("Not a Anagram")
+}
+
+//Anagram - optimized approach
+str1count = {}
+str2count = {}
+let isAnagram = true;
+
+var str1 = "listen"
+var str2 = "hello"
+
+for(let ch1 of str1){
+    if(str1count[ch1] == undefined){
+        str1count[ch1]=1
+    } else {
+        str1count[ch1]++
+    }
+}
+
+for(let ch2 of str2){
+    if(str2count[ch2] == undefined){
+        str2count[ch2]=1
+    } else {
+        str2count[ch2]++
+    }
+}
+
+for(let key in str1count){
+    if(str1count[key]!==str2count[key]){
+        isAnagram = false;
+        break;
+        
+    } 
+}
+
+if(isAnagram){
+    console.log("Anagram")
+} else {
+    console.log("Not a Anagram")
+}
+
+//vowels
+var str = "Javascript programming language"
+var strL = str.toLowerCase()
+var count =0;
+
+for(let i=0;i<strL.length;i++){
+    if(str[i]=='a' || str[i]=='e'|| str[i]=='i' || str[i]=='o' || str[i]=='u' )
+        count++
+}
+
+console.log(count)
+
+//Q no 16
+
+    var num = "1020330120"
+    var result = ""
+    var count = 0
+
+    for(let val of num){
+        if(val!=="0"){
+            result+=val
+        } 
+        if(val=="0"){
+            count++
+        }
+        
+
+
+    }
+    for(let i=0;i<count;i++){
+        result+="0"
+    }
+
+
+console.log(result)
+console.log(count)
+
+
+
 
 
 
