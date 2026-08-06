fd5ecce9cef1d0dc3abec11407cc97381fd85089
Praveen <praveengu19@gmail.com>
2026-08-06 20:09:36 +0530

    pattern problems

--- problemss.js
+++ problemss.js
@@ -1,2 +1,21 @@
+let row = ""
 
+for(let i=1;i<=5;i++){ //row
+    row = ""
+
+    for(let j=1;j<=5-i;j++){ //spaces
+    row += " "
+    }
+
+      for(let k=1;k<=i;k++){ //stars
+        row+="*"
+    }
+
+    console.log(row)
+
+
+}
+
+
+  
 
