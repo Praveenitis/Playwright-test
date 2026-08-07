20856ee9c887b778ed9e99f0abea339c5fbae3ae
Praveen <praveengu19@gmail.com>
2026-08-07 18:09:32 +0530

    diamonds,prymids patterns

--- problemss.js
+++ problemss.js
@@ -7,14 +7,27 @@ for(let i=1;i<=5;i++){ //row
     row += " "
     }
 
-      for(let k=1;k<=i;k++){ //stars
+      for(let k=1;k<=2*i-1;k++){ //stars
         row+="*"
     }
 
-    console.log(row)
+
+}
+
+for(let i=1;i<=5;i++){ //row
+    row = ""
+
+    for(let j=5;j<=j-i;j++){ //spaces
+    row += " "
+    }
+
+      for(let k=1;k<=2*i-1;k++){ //stars
+        row+="*"
+    }
 
 
 }
+
 
 
   
