513b0b2925aee3f0bf82fb56ad41124159c4e447
Praveen <praveengu19@gmail.com>
2026-08-07 18:06:11 +0530

    common actions file

---
+++ utils/common.js
@@ -0,0 +1,35 @@
+import {expect} from "@playwright/test"
+
+export class CommonActions{
+    constructor(page){
+        this.page = page;
+
+    }
+
+    async Click(locator){
+        await this.page.locator(locator).click()
+    }
+
+    async Fillaction(locator,value){
+        await this.page.locator(locator).fill(value)
+    }
+
+    async waitForLocator(locator){
+        await this.page.locator(locator).waitFor()
+    }
+
+    async waitForSelectors(locator){
+        await this.page.waitForSelector(locator)
+    }
+    async tohaveUrl(value){
+        await expect(this.page).toHaveURL(value)
+    }
+
+    async getText(locator){
+        await this.page.locator(locator).textContent()
+    }
+
+    async isVisible(locator){
+        await expect(this.page.locator(locator)).toBeVisible()
+    }
+}
