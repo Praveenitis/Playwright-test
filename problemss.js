22d5805a4d8d06823a1a6091f861d009c8e512f0
Praveen <praveengu19@gmail.com>
2026-09-02 00:00:19 +0530

    Fix

--- problemss.js
+++ problemss.js
@@ -1,32 +1,17 @@
-let row = ""
+const str = "javascript";
 
-for(let i=1;i<=5;i++){ //row
-    row = ""
+const count = {};
 
-    for(let j=1;j<=5-i;j++){ //spaces
-    row += " "
+for (let char of str) {
+
+    if (count[char]) {
+        count[char]++;
+    } else {
+        count[char] = 1;
     }
-
-      for(let k=1;k<=2*i-1;k++){ //stars
-        row+="*"
-    }
-
-
 }
 
-for(let i=1;i<=5;i++){ //row
-    row = ""
-
-    for(let j=5;j<=j-i;j++){ //spaces
-    row += " "
-    }
-
-      for(let k=1;k<=2*i-1;k++){ //stars
-        row+="*"
-    }
-
-
-}
+console.log(count);
 
 
 
